3012ea53c8cbd943e371287c6d9deb8460be961d
abdul samad <abdulsamad3775@gmail.com>
2024-06-02 17:57:36 +0530

    UI is also ready ..

--- src/cnnClassifier/components/model_evaluation_mlflow.py
+++ src/cnnClassifier/components/model_evaluation_mlflow.py
@@ -64,7 +64,7 @@ class Evaluation:
             )
             # Model registry does not work with file store
             if tracking_url_type_store != "file":
-                print('helooooooooooooooooooooooooooooooooo')
+
                 # Register the model
                 # There are other ways to use the Model Registry, which depends on the use case,
                 # please refer to the doc for more information:
